406e85e5c74c40a3acff18f868056409e6fa64f4
Traun Leyden <traun.leyden@gmail.com>
2024-01-06 16:40:03 +0100

    Write captured png to file

--- screen-ocr-swift-rs/src/lib.rs
+++ screen-ocr-swift-rs/src/lib.rs
@@ -1,17 +1,46 @@
 use swift_rs::{swift, SRString, SRData};
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufWriter;
+
+
 swift!(fn perform_ocr_swift(path: &SRString) -> Option<SRString>);
 swift!(fn screen_capture_swift() -> Option<SRData>);
 
+/**
+ * Given a path to an image, extract the text from it using OCR
+ */
 pub fn extract_text(path: &str) -> String {
     let value: SRString = path.into();
     let result = unsafe { perform_ocr_swift(&value) };
     String::from(result.unwrap().as_str())
 }
 
-pub fn screen_capture() -> () {
+/**
+ * Capture the screen and return a byte array
+ */
+pub fn screen_capture(dest_file: &str) -> () {
     let result = unsafe { screen_capture_swift() };
     let result_vec = result.unwrap().to_vec();
     // Print the length of the vector
     println!("Length of vector: {}", result_vec.len());
+    let _ = write_png_to_file(result_vec, dest_file);
+    
 }
+
+
+/**
+ * Helper function to write a PNG to a file
+ */
+fn write_png_to_file(image_data: Vec<u8>, file_path: &str) -> std::io::Result<()> {
+    // Create a file in write-only mode
+    let file = File::create(file_path)?;
+    let mut buf_writer = BufWriter::new(file);
+
+    // Write the byte array to the file
+    buf_writer.write_all(&image_data)?;
+
+    Ok(())
+}
+
--- screentap-app/src-tauri/src/main.rs
+++ screentap-app/src-tauri/src/main.rs
@@ -5,13 +5,28 @@ extern crate screen_ocr_swift_rs;
 use screen_ocr_swift_rs::extract_text;
 use screen_ocr_swift_rs::screen_capture;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use chrono::Local;
+use std::path::{Path, PathBuf};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
-    screen_capture();
-    let ocr_text = extract_text("/Users/tleyden/Development/screentap/screentap-app/screentap_test_screenshot.png");
+    let timestamp_filename = generate_filename("png");
+    let target_file_path = Path::new("/Users/tleyden/Development/screentap/dataset").join(timestamp_filename);
+    screen_capture(target_file_path.to_str().unwrap());
+    // let ocr_text = extract_text("/Users/tleyden/Development/screentap/screentap-app/screentap_test_screenshot.png");
+    let ocr_text = extract_text(target_file_path.to_str().unwrap());
     let result = format!("Hello, {} - {}", name, ocr_text);
     result
+}
+
+/**
+ * Helper function to generate a filename based on the current time
+ */
+fn generate_filename(extension: &str) -> PathBuf {
+    let now = Local::now();
+    let formatted_time = now.format("%Y_%m_%d_%H_%M_%S").to_string();
+    let filename = format!("{}.{}", formatted_time, extension);
+    PathBuf::from(filename)
 }
 
 fn main() {
