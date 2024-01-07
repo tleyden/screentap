4293b879c7560861462c9b723103c386af2a345f
Traun Leyden <traun.leyden@gmail.com>
2024-01-07 11:18:16 +0100

    Align timestamps image file

--- screentap-app/src-tauri/src/utils.rs
+++ screentap-app/src-tauri/src/utils.rs
@@ -1,24 +1,13 @@
-use std::fs::File;
-use std::io::Write;
-use std::path::{Path, PathBuf};
-use chrono::{DateTime, Local};
+use std::path::PathBuf;
+use chrono::NaiveDateTime;
 
 
 /**
  * Helper function to generate a filename based on the current time
  */
-pub fn generate_filename(now: DateTime<Local>, extension: &str) -> PathBuf {
+pub fn generate_filename(now: NaiveDateTime, extension: &str) -> PathBuf {
 
     let formatted_time = now.format("%Y_%m_%d_%H_%M_%S").to_string();
     let filename = format!("{}.{}", formatted_time, extension);
     PathBuf::from(filename)
-}
-
-/**
- * Helper function to write a string to a file
- */
-pub fn write_string_to_file<P: AsRef<Path>>(file_path: P, content: &str) -> std::io::Result<()> {
-    let mut file = File::create(file_path)?;
-    file.write_all(content.as_bytes())?;
-    Ok(())
 }
