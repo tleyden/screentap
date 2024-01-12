8d656eff2aa38d18ad4765d6082ae983189c03ab
Traun Leyden <traun.leyden@gmail.com>
2024-01-12 11:12:42 +0100

    Rename Greet -> SearchScreenshots

--- screentap-app/src/App.vue
+++ screentap-app/src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+import SearchScreenshots from "./components/SearchScreenshots.vue";
 </script>
 
 <template>
   <div class="container">
     <h1>Welcome to Screentap!</h1>
     
-    <Greet />
+    <SearchScreenshots />
   </div>
 </template>
 
