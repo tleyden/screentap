c23c1e9e93722d5b54e9a6f1641fea98491cb73c
Traun Leyden <traun.leyden@gmail.com>
2024-03-09 22:20:12 +0100

    Resize in swift invoked from rust works

--- screen-ocr-swift-rs/src/lib.rs
+++ screen-ocr-swift-rs/src/lib.rs
@@ -9,6 +9,7 @@ swift!(fn screen_capture_swift() -> Option<SRData>);
 swift!(fn write_images_in_dir_to_mp4_swift(directory_path: &SRString, target_filename: &SRString, use_bitrate_key: Bool) -> ());
 swift!(fn extract_frame_from_mp4_swift(mp4_path: &SRString, frame_id: Int) -> Option<SRData>);    
 swift!(fn get_frontmost_app_swift() -> SRString);
+swift!(fn resize_image_swift(image: SRData) ->  Option<SRData>);
 
 
 pub fn extract_frame_from_mp4(mp4_path: &str, frame_id: isize) -> Option<SRData> {
@@ -19,6 +20,22 @@ pub fn extract_frame_from_mp4(mp4_path: &str, frame_id: isize) -> Option<SRData>
 }
 
 
+pub fn resize_image(png_data: Vec<u8>) -> Vec<u8> {
+
+    // let str: &str = "hello";
+    // let bytes: Vec<u8> = str.as_bytes().to_vec();
+
+    // let swift_byte: SRData = SRData::from(&bytes);
+    // let swift_byte: SRData = SRData::from(&*bytes);
+
+    // Convert the vector to a SRData
+    let image = SRData::from(&*png_data);
+
+    let result = unsafe { resize_image_swift(image) };
+    
+    result.unwrap().to_vec()
+
+}
 
 /**
  * Given a path to a directory of images, write them to an mp4
--- screentap-app/src-tauri/src/main.rs
+++ screentap-app/src-tauri/src/main.rs
@@ -209,6 +209,16 @@ fn setup_handler(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error +
             let screenshot_result = screenshot::save_screenshot(app_data_dir.as_path(), db_filename_path);
             match screenshot_result {
                 Ok(screenshot::ScreenshotSaveResult { png_data, ocr_text, png_image_path, screenshot_id}) => {
+
+                    println!("Resizing screenshot with swift...");
+                    let resized_img = screen_ocr_swift_rs::resize_image(png_data.clone());
+
+                    // Write resized image to disk
+                    let path = Path::new("/tmp/resized_rust.png");
+                    std::fs::write(path, &resized_img).unwrap();
+                    println!("Wrote resized image to disk: /tmp/resized_rust.png");
+
+
                     // Invoke plugins
                     // TODO: any way to avoid this confusing "ref mut" stuff?
                     if let Some(ref mut focus_guard) = focus_guard_option {
